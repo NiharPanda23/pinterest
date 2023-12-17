3a7cc0e6ef68888f71a506b17b4858dfcb8f8365
Nihar R Panda <110398042+NiharPanda23@users.noreply.github.com>
2023-12-17 19:53:56 +0530

    Backend passport setup and login page setupand, posts and user shema complete and plm setup coplete

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const userModel = require("./users");
-const postsModel = require("./post");
+const postsModel = require("./posts");
 const passport = require('passport');
 const localStrategy = require('passport-local');
 
@@ -9,7 +9,11 @@ passport.authenticate(new localStrategy(userModel.authenticate()));
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index');
+  res.render("login");
+});
+
+router.get('/profile', isLoggedIn, function(req, res) {
+   res.send("Welcome to profile");
 });
 
 router.post("/register", function(req, res) {
@@ -29,9 +33,24 @@ router.post("/register", function(req, res) {
   })
 });
 
-router.post("/login", function(req, res) {
+router.post("/login", passport.authenticate("local", {
+  successRedirect: "/profile",
+  failureRedirect: "/"
+}), function(req, res) {
   
 });
+
+router.get("/logout", function(req, res){
+  req.logout(function(err) {
+    if (err) { return next(err); }
+    res.redirect('/');
+  });
+});
+
+function isLoggedIn(req, res, next){
+  if(req.isAuthenticated()) return next();
+  res.redirect("/")
+}
 
 
 module.exports = router;
